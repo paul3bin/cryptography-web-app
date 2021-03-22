0496c35c4aec1e20020df4bca282a2b5e3e91612
Ebin Paul <ebinpaul@outlook.com>
2021-03-22 09:39:36 +0530

    Update api_service.js

---
+++ src/api_service.js
@@ -0,0 +1,48 @@
+export class API {
+  static caesarAPI(body) {
+    return fetch(`https://cryptography-flask-api.herokuapp.com/caesar`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
+  }
+
+  static morseCodeAPI(body) {
+    return fetch(`https://cryptography-flask-api.herokuapp.com/morsecode`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
+  }
+
+  static vignereCipherAPI(body) {
+    return fetch(`https://cryptography-flask-api.herokuapp.com/vignere`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
+  }
+
+  static runningKeyCipherAPI(body) {
+    return fetch(
+      `https://cryptography-flask-api.herokuapp.com/runningkeycipher`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    ).then((resp) => resp.json());
+  }
+}
